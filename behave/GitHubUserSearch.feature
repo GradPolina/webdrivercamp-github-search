c4b553dfb4a8083ae1f6231d8d9879401ef9bded
GradPolina <Gradinar_polina@yahoo.com>
2024-07-26 19:26:09 -0500

    Need to work

--- behave/GitHubUserSearch.feature
+++ behave/GitHubUserSearch.feature
@@ -1,1 +1,21 @@
+Feature: Search for users' data on GitHub to track changes in real time
+
+  Background:
+    Given Navigate to https://gh-users-search.netlify.app/
+    When UI: Search for GradPolina
+
+  Scenario: Navigate to the page
+
+  Scenario: Search for User
+
+  Scenario: Verify total number of Repos against API
+    Then GitHub Integration API: verify total number of Repos
+
+  Scenario: Verify number of followers against API
+    And API: send GET request to users/<username>/followers?per_page=100
+    And API: verify status code is 200
+    Then GitHub Integration API: verify fields values
+
+
+
 
--- behave/steps/GitHubUserSearch.py
+++ behave/steps/GitHubUserSearch.py
@@ -1,11 +1,38 @@
-from behave import *
+from behave import step
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import requests
 
-use_step_matcher("re")
+from behave.components.base import Base
 
 
-@given("Navigate to https://gh-users-search\.netlify\.app/")
+@step('Navigate to {url}')
+def step_impl(context, url):
+    context.browser.get(url)
+    time.sleep(2)
+
+
+@step('UI: Search for {username}')
+def step_impl(context, username):
+    base = Base(context.browser)
+    search_box_xpath = '//input[@data-testid="search-bar"]'
+    search_box = base.find_element(search_box_xpath)
+    search_box.clear()
+    search_box.send_keys(username)
+    search_box.send_keys(Keys.RETURN)
+    time.sleep(2)
+
+
+@step("GitHub Integration API: verify total number of Repos")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: Given Navigate to https://gh-users-search.netlify.app/')
+    base = Base(context.browser)
+    repo_count_xpath = '//h3[contains(text(),"8")]'
+    repo_count = base.find_element(repo_count_xpath)
+    ui_repo_count = int(repo_count.text)
+    url = 'https://api.github.com/users/{username}'
+    username = 'GradPolina'
+    response = requests.get(url)
+    api_repo_count = response.json()
+
+    assert ui_repo_count == api_repo_count, f"UI repo count ({ui_repo_count}) does not match API repo count ({api_repo_count})"
